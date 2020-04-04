eec9bc249cd898832631fc6ad0f9203ed804de97
Javier de Paula <javier_depaula@ultimatesoftware.com>
2020-04-04 00:09:58 -0400

    bah

--- scripts/start-remote-deploy.sh
+++ scripts/start-remote-deploy.sh
@@ -9,14 +9,12 @@ echo "Creating Login information"
 mkdir -p ~/.ssh
 eval `ssh-agent -s`
 
-echo "Disabling fingerprint check"
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $DEPLOY_USER@$SERVER
-
 echo "Creating private Key"
 echo "$STRATEGY_PK" | base64 --decode > ~/.ssh/strategy.pk
 chmod 400 ~/.ssh/strategy.pk
 
 echo "Executing script on Server"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $DEPLOY_USER@$SERVER
 ssh -i ~/.ssh/strategy.pk $DEPLOY_USER@$SERVER 'bash -s' < ./scripts/deploy.sh
 
 echo "Finishing deploy"
