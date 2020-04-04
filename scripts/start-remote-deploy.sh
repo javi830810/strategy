8df1255b786842013716d58cf2214a1b92194880
Javier de Paula <javier_depaula@ultimatesoftware.com>
2020-04-03 23:34:21 -0400

    Travis should work

--- scripts/start-remote-deploy.sh
+++ scripts/start-remote-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$pwd
+pwd
 
 #Start remote deploy
 echo "Starting remote deploy"
@@ -10,13 +10,13 @@ mkdir -p ~/.ssh
 eval `ssh-agent -s`
 
 echo "Creating private Key"
-echo "$STRATEGY_PK" >> ~/.ssh/strategy.pk
+echo "$STRATEGY_PK" | base64 --decode > ~/.ssh/strategy.pk
 chmod 600 ~/.ssh/strategy.pk
 ssh-add ~/.ssh/strategy.pk
 
 echo "Copying sensitive variables to Server"
-scp -i ~/.ssh/strategy.pk deploy.sh $DEPLOY_USER@$SERVER:/root/strategy
+scp -i ~/.ssh/strategy.pk ./scripts/deploy.sh $DEPLOY_USER@$SERVER:/root/strategy
 
-ssh -i ~/.ssh/strategy.pk $DEPLOY_USER@$SERVER 'bash -s' < deploy.sh
+ssh -i ~/.ssh/strategy.pk $DEPLOY_USER@$SERVER 'bash -s' < /root/strategy/deploy.sh
 
 echo "Finishing deploy"
